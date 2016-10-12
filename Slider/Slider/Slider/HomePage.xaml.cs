a6533f53d4189ae17ffd6b100a3f2dde961f5063
rukantabula <brukantabula@gmail.com>
2016-10-12 11:27:19 +0200

    slider

---
+++ Slider/Slider/Slider/HomePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Slider
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void MainSlider_OnValueChanged(Object sender
+            , ValueChangedEventArgs e)
+        {
+            MainLabel.Text = MainSlider.Value.ToString();
+        }
+    }
+}
