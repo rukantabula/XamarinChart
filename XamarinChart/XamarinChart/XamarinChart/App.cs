ad7c80d7bfb679ce14de740ff4313c23933cf883
rukantabula <brukantabula@gmail.com>
2016-10-12 10:53:23 +0200

    Pie Chart

---
+++ XamarinChart/XamarinChart/XamarinChart/App.cs
@@ -0,0 +1,34 @@
+﻿using CrossPieCharts.FormsPlugin.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinChart
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new CrossPieChartSample().GetPageWithPieChart();
+
+        }
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
